2c5ec81449c94cbf5efa75320380364ac3561d85
Tom King <ka6sox@gmail.com>
2014-06-16 21:09:25 -0700

    add Mk1 repo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-Pongsat-Flight-Computer
-=======================
+bcc_sdr
+=======
+
+SDR daughtercard for BCC S6 FPGA board
+
+All Files and Programmes Copyright 2014 Eric Brombaugh and/or Tom King
+
+All Files and Programmes licensed under GPL v2 (See License.txt)
---
+++ pfc-mk1/pfc_MkI.cmd
@@ -0,0 +1,241 @@
+# Pin name action command file
+
+# Start of element TP35
+ChangePinName(TP35, 1, 1)
+
+# Start of element TP36
+ChangePinName(TP36, 1, 1)
+
+# Start of element FB1
+ChangePinName(FB1, 2, 2)
+ChangePinName(FB1, 1, 1)
+
+# Start of element C9
+ChangePinName(C9, 1, 1)
+ChangePinName(C9, 2, 2)
+
+# Start of element C8
+ChangePinName(C8, 1, +)
+ChangePinName(C8, 2, -)
+
+# Start of element C1
+ChangePinName(C1, 1, +)
+ChangePinName(C1, 2, -)
+
+# Start of element C2
+ChangePinName(C2, 1, 1)
+ChangePinName(C2, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, 1, 1)
+ChangePinName(C3, 2, 2)
+
+# Start of element C7
+ChangePinName(C7, 1, 1)
+ChangePinName(C7, 2, 2)
+
+# Start of element C6
+ChangePinName(C6, 1, 1)
+ChangePinName(C6, 2, 2)
+
+# Start of element C5
+ChangePinName(C5, 1, 1)
+ChangePinName(C5, 2, 2)
+
+# Start of element C4
+ChangePinName(C4, 1, 1)
+ChangePinName(C4, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 2, 2)
+ChangePinName(R2, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 2, 2)
+ChangePinName(R1, 1, 1)
+
+# Start of element TP34
+ChangePinName(TP34, 1, 1)
+
+# Start of element TP30
+ChangePinName(TP30, 1, 1)
+
+# Start of element TP21
+ChangePinName(TP21, 1, 1)
+
+# Start of element TP20
+ChangePinName(TP20, 1, 1)
+
+# Start of element TP19
+ChangePinName(TP19, 1, 1)
+
+# Start of element TP18
+ChangePinName(TP18, 1, 1)
+
+# Start of element TP17
+ChangePinName(TP17, 1, 1)
+
+# Start of element TP16
+ChangePinName(TP16, 1, 1)
+
+# Start of element TP15
+ChangePinName(TP15, 1, 1)
+
+# Start of element TP14
+ChangePinName(TP14, 1, 1)
+
+# Start of element TP10
+ChangePinName(TP10, 1, 1)
+
+# Start of element TP11
+ChangePinName(TP11, 1, 1)
+
+# Start of element TP12
+ChangePinName(TP12, 1, 1)
+
+# Start of element TP13
+ChangePinName(TP13, 1, 1)
+
+# Start of element TP9
+ChangePinName(TP9, 1, 1)
+
+# Start of element TP8
+ChangePinName(TP8, 1, 1)
+
+# Start of element TP7
+ChangePinName(TP7, 1, 1)
+
+# Start of element TP4
+ChangePinName(TP4, 1, 1)
+
+# Start of element TP3
+ChangePinName(TP3, 1, 1)
+
+# Start of element TP5
+ChangePinName(TP5, 1, 1)
+
+# Start of element TP6
+ChangePinName(TP6, 1, 1)
+
+# Start of element TP27
+ChangePinName(TP27, 1, 1)
+
+# Start of element TP28
+ChangePinName(TP28, 1, 1)
+
+# Start of element TP24
+ChangePinName(TP24, 1, 1)
+
+# Start of element TP23
+ChangePinName(TP23, 1, 1)
+
+# Start of element TP22
+ChangePinName(TP22, 1, 1)
+
+# Start of element TP25
+ChangePinName(TP25, 1, 1)
+
+# Start of element TP26
+ChangePinName(TP26, 1, 1)
+
+# Start of element TP1
+ChangePinName(TP1, 1, 1)
+
+# Start of element TP32
+ChangePinName(TP32, 1, 1)
+
+# Start of element TP31
+ChangePinName(TP31, 1, 1)
+
+# Start of element TP29
+ChangePinName(TP29, 1, 1)
+
+# Start of element TP33
+ChangePinName(TP33, 1, 1)
+
+# Start of element TP2
+ChangePinName(TP2, 1, 1)
+
+# Start of element D1
+ChangePinName(D1, 1, cathode)
+ChangePinName(D1, 2, anode)
+
+# Start of element B1
+ChangePinName(B1, 2, -)
+ChangePinName(B1, 1, +)
+
+# Start of element U2
+ChangePinName(U2, 5, /WP)
+ChangePinName(U2, 4, /CS)
+ChangePinName(U2, 3, /RESET)
+ChangePinName(U2, 2, SCK)
+ChangePinName(U2, 1, SI)
+ChangePinName(U2, 6, VCC)
+ChangePinName(U2, 7, GND)
+ChangePinName(U2, 8, SO)
+
+# Start of element U1
+ChangePinName(U1, 53, PC12)
+ChangePinName(U1, 52, PC11)
+ChangePinName(U1, 51, PC10)
+ChangePinName(U1, 50, PA15)
+ChangePinName(U1, 46, PA13)
+ChangePinName(U1, 44, PA11)
+ChangePinName(U1, 45, PA12)
+ChangePinName(U1, 43, PA10)
+ChangePinName(U1, 32, VDD_1)
+ChangePinName(U1, 31, VSS_1)
+ChangePinName(U1, 30, PB11)
+ChangePinName(U1, 29, PB10)
+ChangePinName(U1, 16, PA2)
+ChangePinName(U1, 15, PA1)
+ChangePinName(U1, 14, PA0_WKUP)
+ChangePinName(U1, 13, VDDA)
+ChangePinName(U1, 64, VDD_3)
+ChangePinName(U1, 63, VSS_3)
+ChangePinName(U1, 62, BP9)
+ChangePinName(U1, 61, PB8)
+ChangePinName(U1, 60, BOOT0)
+ChangePinName(U1, 59, PB7)
+ChangePinName(U1, 58, PB6)
+ChangePinName(U1, 57, PB5)
+ChangePinName(U1, 38, PC7)
+ChangePinName(U1, 37, PC6)
+ChangePinName(U1, 36, PB15)
+ChangePinName(U1, 35, PB14)
+ChangePinName(U1, 34, PB13)
+ChangePinName(U1, 33, PB12)
+ChangePinName(U1, 28, PB2)
+ChangePinName(U1, 27, PB1)
+ChangePinName(U1, 26, PB0)
+ChangePinName(U1, 25, PC5)
+ChangePinName(U1, 56, PB4)
+ChangePinName(U1, 55, PB3)
+ChangePinName(U1, 54, PD2)
+ChangePinName(U1, 49, PA14)
+ChangePinName(U1, 48, VDD_2)
+ChangePinName(U1, 47, VSS_2)
+ChangePinName(U1, 42, PA9)
+ChangePinName(U1, 41, PA8)
+ChangePinName(U1, 40, PC9)
+ChangePinName(U1, 39, PC8)
+ChangePinName(U1, 10, PC2)
+ChangePinName(U1, 9, PC1)
+ChangePinName(U1, 8, PC0)
+ChangePinName(U1, 7, NRST)
+ChangePinName(U1, 6, PH1-OSC_OUT)
+ChangePinName(U1, 5, PH0-OSC_IN)
+ChangePinName(U1, 4, PC15-OSC32_OUT)
+ChangePinName(U1, 3, PC14-OSC32_IN)
+ChangePinName(U1, 2, PC13_WKUP2)
+ChangePinName(U1, 1, VLCD)
+ChangePinName(U1, 24, PC4)
+ChangePinName(U1, 23, PA7)
+ChangePinName(U1, 22, PA6)
+ChangePinName(U1, 21, PA5)
+ChangePinName(U1, 20, PA4)
+ChangePinName(U1, 19, VDD_4)
+ChangePinName(U1, 18, VSS_4)
+ChangePinName(U1, 17, PA3)
+ChangePinName(U1, 12, VSSA)
+ChangePinName(U1, 11, PC3)
